90eb6608877a46d8e19fe533c486603bbab676bc
David Kuo <s50407s@gmail.com>
2016-02-24 01:41:16 +0800

    Enable auto rebuild docs

--- travis-build.sh
+++ travis-build.sh
@@ -6,13 +6,13 @@ rev=$(git rev-parse --short HEAD)
 
 bundle exec rake docs docs:tidy
 
-git config --global user.email "bcardiff@manas.com.ar"
-git config --global user.name "Travis on behalf Brian J. Cardiff"
+git config --global user.email "me@davy.tw"
+git config --global user.name "Travis on behalf David Kuo"
 
-git remote add upstream "https://$GH_TOKEN@github.com/manastech/crystal"
+git remote add upstream "https://$GH_TOKEN@github.com/crystal-tw/crystal-tw.github.io"
 git fetch upstream
-git reset upstream/gh-pages
+git reset upstream/master
 
 git add -A docs/
 git commit -m "rebuild docs at ${rev} [ci skip]" || true
-git push -q upstream HEAD:gh-pages
+git push -q upstream HEAD:master
