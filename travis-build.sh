7090dcb03f25b816c0298c6e80a7c77242e93808
David Kuo <s50407s@gmail.com>
2016-06-17 03:29:14 +0800

    Update travis to make master following translation

--- travis-build.sh
+++ travis-build.sh
@@ -4,14 +4,15 @@ set -e
 
 rev=$(git rev-parse --short HEAD)
 
-bundle exec rake docs docs:tidy
-
 git config --global user.email "me@davy.tw"
 git config --global user.name "Travis on behalf David Kuo"
 
 git remote add upstream "https://$GH_TOKEN@github.com/crystal-tw/crystal-tw.github.io"
 git fetch upstream
-git reset upstream/master
+git checkout upstream/master
+git merge origin/translation --no-commit --no-edit --no-ff -s recursive -Xtheirs || true
+
+bundle exec rake docs docs:tidy
 
 git add -fA docs/
 git commit -m "rebuild docs at ${rev} [ci skip]" || true
